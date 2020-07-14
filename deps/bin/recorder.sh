ab657eda5699e27119668479eb069fde722d8aa1
Darren Corbett <corbsoft@gmail.com>
2020-07-14 07:09:31 +0100

    set path

--- deps/bin/recorder.sh
+++ deps/bin/recorder.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-JAVA="/opt/java/openjdk/bin/java"
+JAVA=/opt/java/openjdk/bin/java
 
 OLDDIR=`pwd`
 BIN_DIR=`dirname $0`
